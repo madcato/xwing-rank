38a89f74ebf9182d19d17934d16228277870854d
Daniel Vela <daniel.vela@hp.com>
2014-03-28 13:14:07 +0100

    Initial local commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XwingRank::Application.config.secret_key_base = 'e115c9aa61f8a44b7b96c9a646efb9260c67459e1633a84fd3894c7766722a8c3215ddba66dfdaff1fc023c245c1c7747fea33274425c67bf93b1bc51db3e41d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+XwingRank::Application.config.session_store :cookie_store, key: '_xwing-rank_session'
