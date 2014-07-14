cf5e71f8c8c04f364edcbc2eed1805cc960ef85e
Daniel Vela <daniel.vela@hp.com>
2014-07-14 13:10:43 +0200

    Added a players find method to matches creation

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -1,2 +1,7 @@
 module MatchesHelper
+  
+  # TODO this method must be edited to return only the players of the tournament
+  def collectPlayersForTournmanet(tourny)
+    Player.all.collect {|p| [ p.name, p.id ] }
+  end
 end
