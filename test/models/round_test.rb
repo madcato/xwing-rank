9f78e6a62eb7f27a9aff8003f9a6adcbcbc2e197
Daniel Vela <daniel.vela@hp.com>
2014-08-06 14:39:16 +0200

    Added player, round and tourney tests

--- test/models/round_test.rb
+++ test/models/round_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class RoundTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "order must exists" do
+    round = Round.new
+    assert_not round.save, "Round saved without order field"
+  end
 end
