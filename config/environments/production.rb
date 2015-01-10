523290a07c382189fcfeedfe6582ac17afcbccdd
Daniel Vela <dani_vela@me.com>
2015-01-10 11:46:27 +0100

    To send conformation mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ XwingRank::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  
+  config.action_mailer.raise_delivery_errors = true 
+  config.action_mailer.default_url_options = { host: 'http://www.xwingrankor.com' } 
+  config.action_mailer.asset_host = 'http://www.xwingrankor.com' 
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.smtp_settings = { address: 'smtp.gmail.com', 
+    port: 587, user_name: 'veladan@gmail.com', password: 'C4nd3r3l', 
+    authentication: 'plain', 
+    enable_starttls_auto: true }
 end
