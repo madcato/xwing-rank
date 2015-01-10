b04c798c859857bec81876f1bb07f98f25315926
Daniel Vela <dani_vela@me.com>
2015-01-10 12:23:40 +0100

    Readied mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,16 @@ XwingRank::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  
-  config.action_mailer.raise_delivery_errors = true 
+  config.action_mailer.delivery_method = :smtp 
   config.action_mailer.default_url_options = { host: 'http://www.xwingrankor.com' } 
   config.action_mailer.asset_host = 'http://www.xwingrankor.com' 
-  config.action_mailer.delivery_method = :smtp 
-  config.action_mailer.smtp_settings = { address: 'smtp.gmail.com', 
-    port: "587", user_name: 'veladan@gmail.com', password: 'none', 
-    authentication: 'plain', 
-    enable_starttls_auto: true }
+  config.action_mailer.smtp_settings = { 
+    address: 'smtp.gmail.com', 
+    port: 587, 
+    domain: 'http://www.xwingrankor.com',
+    user_name: 'veladan@gmail.com', 
+    password: 'M4nd4rt3', 
+    authentication: :plain, 
+    enable_starttls_auto: true
+  }
 end
