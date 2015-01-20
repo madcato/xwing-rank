9cb2dec99c81bc49c3c60c4dd6e4c135f6630b35
Daniel Vela <dani_vela@me.com>
2015-01-20 10:21:07 +0100

    project closed

--- config/routes.rb
+++ config/routes.rb
@@ -1,42 +1,44 @@
 XwingRank::Application.routes.draw do
   
-  resources :rankings
-
-  get 'faq' => 'public_tourney#faq'
-  
-  devise_for :users
-  devise_for :admins
-  
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  resources :tourneys do
-    get 'report' => 'tourneys#report'
-    get 'dropPlayer/:player_id' => 'rounds#dropPlayer'
-    get 'undropPlayer/:player_id' => 'rounds#undropPlayer'
-    get 'removePlayer/:player_id' => 'rounds#removePlayer'
-    get 'newPlayer/' => 'rounds#newPlayer'
-    get 'printInscribed/' => 'tourneys#printInscribed'
-    get 'printRanking/' => 'tourneys#printRanking'
-    post 'createInscription/' => 'rounds#createInscription'
-    post 'createAndSeedRound/' => 'rounds#createAndSeedRound'
-    resources :rounds do
-      get 'seedRound/' => 'rounds#seedRound'
-      get 'printRound/' => 'rounds#printRound'
-      resources :matches
-    end
-  end
-  
-  resources :players
-
-  
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'players#list'
-
-  get 'simple' => 'players#simple'
-  
-  get 'tourney/:publicId' => 'public_tourney#index'
+  # resources :rankings
+ #
+ #  get 'faq' => 'public_tourney#faq'
+ #
+ #  devise_for :users
+ #  devise_for :admins
+ #
+ #  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+ #  resources :tourneys do
+ #    get 'report' => 'tourneys#report'
+ #    get 'dropPlayer/:player_id' => 'rounds#dropPlayer'
+ #    get 'undropPlayer/:player_id' => 'rounds#undropPlayer'
+ #    get 'removePlayer/:player_id' => 'rounds#removePlayer'
+ #    get 'newPlayer/' => 'rounds#newPlayer'
+ #    get 'printInscribed/' => 'tourneys#printInscribed'
+ #    get 'printRanking/' => 'tourneys#printRanking'
+ #    post 'createInscription/' => 'rounds#createInscription'
+ #    post 'createAndSeedRound/' => 'rounds#createAndSeedRound'
+ #    get 'elimination/' => 'tourneys#elimination'
+ #    post 'startElimination/' => 'tourneys#startElimination'
+ #    resources :rounds do
+ #      get 'seedRound/' => 'rounds#seedRound'
+ #      get 'printRound/' => 'rounds#printRound'
+ #      resources :matches
+ #    end
+ #  end
+ #
+ #  resources :players
+ #
+ #
+ #  # The priority is based upon order of creation: first created -> highest priority.
+ #  # See how all your routes lay out with "rake routes".
+ #
+ #  # You can have the root of your site routed with "root"
+ #  root 'players#list'
+ #
+ #  get 'simple' => 'players#simple'
+ #
+ #  get 'tourney/:publicId' => 'public_tourney#index'
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
